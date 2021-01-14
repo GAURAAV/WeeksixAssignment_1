3a5df58f2bd748ea5585b3531d34d583b21928c2
gaurav <43298069+GAURAAV@users.noreply.github.com>
2021-01-14 22:21:14 +0545

    Inital commit

---
+++ app/src/main/java/com/gauravthapa/week6assignment1/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.gauravthapa.week6assignment1.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
