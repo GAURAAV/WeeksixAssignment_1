136b89049a93a2084a981101477ed37acce5a246
gaurav <43298069+GAURAAV@users.noreply.github.com>
2021-01-15 14:01:53 +0545

    Initial commit

--- app/src/main/java/com/gauravthapa/week6assignment1/ui/login/LoggedInUserView.kt
+++
@@ -1,9 +0,0 @@
-package com.gauravthapa.week6assignment1.ui.login
-
-/**
- * User details post authentication that is exposed to the UI
- */
-data class LoggedInUserView(
-    val displayName: String
-    //... other data fields that may be accessible to the UI
-)
